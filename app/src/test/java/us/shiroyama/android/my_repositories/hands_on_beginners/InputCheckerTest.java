9c3791336833c7c0d9ad28c0720ab60e696136de
kktyu <ku.k669500@gmail.com>
2019-11-25 16:00:41 +0900

    〜完了〜 2. はじめてのUnit Test

--- app/src/test/java/us/shiroyama/android/my_repositories/hands_on_beginners/InputCheckerTest.java
+++ app/src/test/java/us/shiroyama/android/my_repositories/hands_on_beginners/InputCheckerTest.java
@@ -1,9 +1,8 @@
 package us.shiroyama.android.my_repositories.hands_on_beginners;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Local Unit Test for {@link InputChecker}
@@ -12,65 +11,71 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 
 public class InputCheckerTest {
-  private InputChecker inputChecker;
+    private InputChecker inputChecker;
 
-  /**
-   * ここに {@link InputChecker} の初期化処理を書いて各テストケースで共用しよう。
-   */
-  @Before
-  public void setUp() throws Exception {
-  }
+    /**
+     * ここに {@link InputChecker} の初期化処理を書いて各テストケースで共用しよう。
+     */
+    @Before
+    public void setUp() throws Exception {
+        inputChecker = new InputChecker();
+    }
 
-  /**
-   * 正しい入力で<code>true</code>を受け取るテストケースを書いてみよう。
-   * <p>
-   * 例: <code>String input = "srym";</code>
-   */
-  @Test
-  public void isValid() throws Exception {
-    String input = "srym";
-  }
+    /**
+     * 正しい入力で<code>true</code>を受け取るテストケースを書いてみよう。
+     * <p>
+     * 例: <code>String input = "srym";</code>
+     */
+    @Test
+    public void isValid() throws Exception {
+        String input = "srym";
+        Assertions.assertThat(inputChecker.isValid(input)).isTrue();
+    }
 
-  /**
-   * 不正な入力（空文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
-   * <p>
-   * 例: <code>String input = "";</code>
-   */
-  @Test
-  public void isValid_inputBlank_resultsFalse() throws Exception {
-    String input = "";
-  }
+    /**
+     * 不正な入力（空文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
+     * <p>
+     * 例: <code>String input = "";</code>
+     */
+    @Test
+    public void isValid_inputBlank_resultsFalse() throws Exception {
+        String input = "";
+        Assertions.assertThat(inputChecker.isValid(input)).isFalse();
+    }
 
-  /**
-   * 不正な入力（記号や半角スペースが含まれた文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
-   * <p>
-   * 例: <code>String input = "abc$";</code>
-   */
-  @Test
-  public void isValid_inputIllegalCharacters_resultsFalse() throws Exception {
-    String input = "abc$";
-  }
+    /**
+     * 不正な入力（記号や半角スペースが含まれた文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
+     * <p>
+     * 例: <code>String input = "abc$";</code>
+     */
+    @Test
+    public void isValid_inputIllegalCharacters_resultsFalse() throws Exception {
+        String input = "abc$";
+        Assertions.assertThat(inputChecker.isValid(input)).isFalse();
+    }
 
-  /**
-   * 不正な入力（規定文字数より少ない文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
-   * <p>
-   * 例: <code>String input = "abc";</code>
-   */
-  @Test
-  public void isValid_inputLessThan4_resultsFalse() throws Exception {
-    String input = "abc";
-  }
+    /**
+     * 不正な入力（規定文字数より少ない文字列）で<code>false</code>を受け取るテストケースを書いてみよう。
+     * <p>
+     * 例: <code>String input = "abc";</code>
+     */
+    @Test
+    public void isValid_inputLessThan4_resultsFalse() throws Exception {
+        String input = "abc";
+        Assertions.assertThat(inputChecker.isValid(input)).isFalse();
+    }
 
-  /**
-   * 不正な入力（null）で<code>NullPointerException</code>が上がることを確認するテストケースを書いてみよう。
-   * <p>
-   * 例: <code>String input = null;</code>
-   * <p>
-   * ヒント: <code>@Test(expected = 例外.class)</code>
-   */
-  @Test
-  public void isValid_inputNull_resultsNPE() throws Exception {
-    String input = null;
-  }
+    /**
+     * 不正な入力（null）で<code>NullPointerException</code>が上がることを確認するテストケースを書いてみよう。
+     * <p>
+     * 例: <code>String input = null;</code>
+     * <p>
+     * ヒント: <code>@Test(expected = 例外.class)</code>
+     */
+    @Test(expected = NullPointerException.class)
+    public void isValid_inputNull_resultsNPE() throws Exception {
+        String input = null;
+        inputChecker.isValid(input);
+    }
 
 }
